a7d6cac1dada3eacd872dc7974da44fdd704b793
Diagilev <diagilev.av@gmail.com>
2022-05-05 18:53:50 +0300

    change Vehicle delayByTime db query

--- src/main/java/com/traffic/api/DAO/DelayDAO.java
+++ src/main/java/com/traffic/api/DAO/DelayDAO.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public interface DelayDAO extends JpaRepository<Delay, Integer> {
-    @Query(value ="SELECT vehicles_delay.id, vehicles_delay.time, vehicles_delay.current_delay, vehicles_history_delay.history_delay FROM db_traffic_lviv.vehicles_delay INNER JOIN db_traffic_lviv.vehicles_history_delay ON time(vehicles_delay.time) = vehicles_history_delay.time and dayofweek(vehicles_delay.time) = vehicles_history_delay.day_of_week where vehicles_delay.time >= ?1 and vehicles_delay.time<=?2 and year = '2020' ORDER BY time", nativeQuery = true)
+    @Query(value ="SELECT vehicles_delay.id, vehicles_delay.time, vehicles_delay.current_delay, vehicles_history_delay.history_delay FROM db_traffic_lviv.vehicles_delay LEFT JOIN db_traffic_lviv.vehicles_history_delay ON time(vehicles_delay.time) = vehicles_history_delay.time and dayofweek(vehicles_delay.time) = vehicles_history_delay.day_of_week and year = '2020' where vehicles_delay.time >=? and vehicles_delay.time<=?  ORDER BY time", nativeQuery = true)
     public List<Delay> getByTime(String startTime, String endTime);
 
     @Query(value ="SELECT id, history_delay, avg(current_delay) as current_delay, date(time) as time FROM db_traffic_lviv.vehicles_delay where date(time) >= ?1 group by date(time) ORDER BY time", nativeQuery = true)
